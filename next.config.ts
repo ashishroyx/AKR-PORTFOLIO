40ae3ed8c87d4b202fb1a69517246b387b6ec211
ASHISH KUMAR ROY <ashishkumarroy2502@gmail.com>
2025-06-12 19:14:56 +0530

    Disable ESLint during build for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+const nextConfig: import('next').NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+
 
 export default nextConfig;
